4f6177830a2cd79ac856bf8b46b146beb1551674
{sSatoshi} <{one_two_3104@yahoo.co.jp}>
2023-11-28 19:10:46 +0900

    mata setting

--- js/main.js
+++ js/main.js
@@ -11,18 +11,46 @@ menuBtn.addEventListener('click', () => {
     }
 })
 
-const animationObjects = document.querySelectorAll('.animation-object');
-
-animationObjects.forEach((animationObject) => {
-    
-    const showAnimation = () => {
-        animationObject.classList.add('animation');
-        // observe.unobserve(animationObject)
+    const showAnimation = (entries, obs) => {
+        entries.forEach((entry) => {
+            if(entry.isIntersecting){
+                entry.target.classList.add('animation');
+                console.log(entry.target)
+                
+                obs.unobserve(entry.target);
+            }
+        })
     }
     
     const animationObserver = new IntersectionObserver(showAnimation);
     
+
+const animationObjects = document.querySelectorAll('.animation-object');
+animationObjects.forEach((animationObject) => {
     animationObserver.observe(animationObject);
-});
+})
+
+
+
+
+
+const header = document.querySelector('.header-wrapper');
+const headerObject = document.querySelector('.headerObject');
+const colorChange = (entries) => {
+
+    if(entries[0].isIntersecting !== true){
+        header.classList.add('header-change');
+    } else {
+        header.classList.remove('header-change');
+    }
+
+    // console.log(entries[0].isIntersecting);
+}
+
+const headerObserver = new IntersectionObserver(colorChange);
+
+headerObserver.observe(headerObject);
+
+
 
 
